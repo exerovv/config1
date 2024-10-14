547ecbb477752f5e5539124123a6f6ca47e87886
Emir Sabirov <rasher.play44@yandex.ru>
2024-10-14 17:04:57 +0300

    Теперь буду писать по русски. Изменена логика работы с .tar файлом, а также добавлена функция cd(ее первая вариация

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import argparse
 import tarfile
 import os
 
-
 def prompt(username, current_path):
     home_path = "/root"
     if current_path == home_path:
@@ -18,25 +17,27 @@ def exit_shell():
 
 def run_shell(username, tar):
     current_path = "/root"
-    tar_files = list_files_in_tar(tar)
 
-    while True:
-        try:
-            command = input(prompt(username, current_path)).strip().split()
-            if not command:
-                continue
-            cmd = command[0]
-            if cmd == 'exit':
+    with tarfile.open(tar, "r") as tar_file:
+        while True:
+            try:
+                command = input(prompt(username, current_path)).strip().split()
+                if not command:
+                    continue
+                cmd = command[0]
+                if cmd == 'exit':
+                    exit_shell()
+                elif cmd == 'ls':
+                    list_directory(current_path, tar_file.getnames())
+                elif cmd == 'cd':
+                    if len(command) > 1:
+                        current_path = change_directory(current_path, command[1], tar_file)
+                    else:
+                        print("cd: missing argument")
+                else:
+                    print(f"{cmd}: command not found")
+            except KeyboardInterrupt:
                 exit_shell()
-            elif cmd == 'ls':
-                for file in tar_files:
-                    relative_path = file[len(current_path):].lstrip('/')
-                    if '/' not in relative_path and relative_path:
-                        print(relative_path)
-            else:
-                print(f"{cmd}: command not found")
-        except KeyboardInterrupt:
-            exit_shell()
 
 
 def parse_args():
@@ -45,9 +46,28 @@ def parse_args():
     parser.add_argument('--tar', required=True, help='Путь к tar-файлу с виртуальной файловой системой')
     return parser.parse_args()
 
-def list_files_in_tar(tar):
-    with tarfile.open(tar, "r") as tar_file:
-        return tar_file.getnames()
+def list_directory(current_path, tar_files):
+    for file in tar_files:
+        relative_path = file[len(current_path):].lstrip('/')
+        if '/' not in relative_path and relative_path:
+            print(relative_path)
+
+
+def change_directory(current_path, target_directory, tar_file):
+    if target_directory == "/":
+        return "/root"
+
+    elif target_directory == "..":
+        if current_path != "/root":
+            return os.path.dirname(current_path)
+        return current_path
+    else:
+        new_path = os.path.join(current_path, target_directory).replace("\\", "/").strip('/')
+        if any(f.startswith(new_path + '/') for f in tar_file.getnames()):
+            return new_path
+        else:
+            print(f"cd: {target_directory}: No such file or directory")
+            return current_path
 
 
 def main():
